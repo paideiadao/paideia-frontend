2fc2765ea611f95ee286c27f86ba1c293ede28af
michaelmirandi <61252938+michaelmirandi@users.noreply.github.com>
2022-05-17 01:04:42 -0400

    Footer and dashboard

--- components/dao/DaoTemplate.tsx
+++ components/dao/DaoTemplate.tsx
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import * as React from "react";
 import { GlobalContext, IGlobalContext } from "../../lib/AppContext";
 import { DarkTheme, LightTheme } from "../../theme/theme";
+import BottomNav from "./nav/BottomNav";
 import Nav from "./nav/Nav";
 import TopNav from "./nav/TopNav";
 
@@ -11,6 +12,12 @@ const DaoTemplate: React.FC<{ subdomain: string }> = (props) => {
     <Box sx={{ display: "flex", alignItems: "flex-start" }}>
       <Nav />
       <TopNav />
+      <Box sx={{width: 'calc(100% - 15rem)', position: 'fixed', top: '4.5rem', left: '15rem'}}>
+        <Box sx={{mb: '1rem', pl: '.5rem', pr: '.5rem'}}>
+          Dashboard here..
+        </Box>
+        <BottomNav/>
+      </Box>
     </Box>
   );
 };
