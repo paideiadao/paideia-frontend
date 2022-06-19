fd7ef7bd2ea361a8235e55bcaa473bea3dc879b1
esot321c <esot321c@gmail.com>
2022-06-19 15:48:00 -0700

    :add: custom card slider component

--- components/CardSlider.tsx
+++ components/CardSlider.tsx
@@ -1,69 +1,161 @@
-import React, { FC } from "react";
-import { Button } from "@mui/material";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React, { FC, useEffect, useState, useRef } from "react";
+import { Button, Container, Box } from "@mui/material";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
-interface CardSliderProps {
-
+const SETTINGS = {
+  navBarTravelling: false,
+  navBarDirection: "",
+  navBarTravelDistance: 150
 }
 
-const CardSlider: FC<CardSliderProps> = ({ children }) => {
+const CardSlider: FC = ({ children }) => {
+  const [marginLeftCalc, setMarginLeftCalc] = useState({ mx: '0px' })
+  const [scrollPosition, setScrollPosition] = useState(0)
+  const [leftDisabled, setLeftDisabled] = useState(false)
+  const [rightDisabled, setRightDisabled] = useState(false)
 
-  var settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
+  const handleScroll = () => {
+    const scroll = document.getElementById("pnProductNav").scrollLeft
+    setScrollPosition(scroll)
+  };
+
+  const determineOverflow = (content: any, container: any) => {
+    const containerMetrics = container.getBoundingClientRect();
+    const containerMetricsRight = Math.floor(containerMetrics.right);
+    const containerMetricsLeft = Math.floor(containerMetrics.left);
+    const contentMetrics = content.getBoundingClientRect();
+    const contentMetricsRight = Math.floor(contentMetrics.right);
+    const contentMetricsLeft = Math.floor(contentMetrics.left);
+    if (containerMetricsLeft > contentMetricsLeft && containerMetricsRight < contentMetricsRight) {
+      setLeftDisabled(false)
+      setRightDisabled(false)
+    } else if (contentMetricsLeft < containerMetricsLeft) {
+      setRightDisabled(true)
+    } else if (contentMetricsRight > containerMetricsRight) {
+      setLeftDisabled(true)
+    } else {
+      setLeftDisabled(true)
+      setRightDisabled(true)
+    }
   }
 
-  const slider = React.useRef(null);
+  const marginFunction = () => {
+    const pnArrowContainer = document.getElementById("pnArrowContainer");
+    const margin = (pnArrowContainer.getBoundingClientRect().left + 24).toString() + 'px'
+    setMarginLeftCalc({ ...marginLeftCalc, mx: margin })
+  }
+
+  useEffect(() => {
+    const pnProductNav = document.getElementById("pnProductNav");
+    pnProductNav.addEventListener("scroll", handleScroll);
+    return () => {
+      pnProductNav.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const pnProductNav = document.getElementById("pnProductNav");
+    const pnProductNavContents = document.getElementById("pnProductNavContents");
+    determineOverflow(pnProductNavContents, pnProductNav)
+  }, [scrollPosition]);
+
+  useEffect(() => {
+    // Add event listener
+    window.addEventListener("resize", marginFunction);
+    // Call handler right away so state gets updated with initial window size
+    marginFunction()
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", marginFunction);
+  }, [])
+
+  const scrollRef = useRef(0)
+  scrollRef.current = scrollPosition
+
+  const [pos, setPos] = useState({
+    left: undefined,
+    x: undefined,
+  })
+
+  const posRef = useRef({
+    left: undefined,
+    x: undefined,
+  })
+  posRef.current = pos
+
+  const mouseMoveHandler = (e: any) => {
+    const pnProductNav = document.getElementById("pnProductNav");
+    pnProductNav.scrollLeft = posRef.current.left - (e.clientX - posRef.current.x);
+    console.log(posRef)
+  console.log(scrollPosition)
+  }
+
+  const mouseUpHandler = (e: any) => {
+    const pnProductNav = document.getElementById("pnProductNav");
+    pnProductNav.style.cursor = 'grab';
+    pnProductNav.style.userSelect = 'none';
+    setPos({
+      ...pos,
+      left: scrollRef.current,
+      x: e.clientX,
+    })
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+  }
+
+  const handleMouseDown = (e: any) => {
+    const pnProductNav = document.getElementById("pnProductNav");
+    pnProductNav.style.cursor = 'grabbing';
+    pnProductNav.style.userSelect = 'none';
+
+    setPos({
+      // The current scroll
+      left: scrollPosition,
+      // Get the current mouse position
+      x: e.clientX,
+    })
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+  }
+  
 
   return (
     <>
-      <Slider ref={slider} {...settings}>
-        {children}
-      </Slider>
-        <Button className="arrow__left" onClick={() => slider?.current?.slickPrev()}>
-          <ArrowBackIosIcon className="arrow__leftIcon" />
+      <Box sx={{
+        /* Make this scrollable when needed */
+        overflowX: 'auto',
+        /* We don't want vertical scrolling */
+        overflowY: 'hidden',
+        cursor: 'grab',
+        /* Make an auto-hiding scroller for the 3 people using a IE */
+        msOverflowStyle: '-ms-autohiding-scrollbar',
+        /* For WebKit implementations, provide inertia scrolling */
+        WebkitOverflowScrolling: 'touch',
+        /* We don't want internal inline elements to wrap */
+        whiteSpace: 'nowrap',
+        /* Remove the default scrollbar for WebKit implementations */
+        '&::-webkit-scrollbar': {
+          display: 'none'
+        },
+      }}
+        id="pnProductNav"
+        onMouseDown={(e) => handleMouseDown(e)}
+      >
+        <Box id="pnProductNavContents" display="flex" sx={{ width: 'min-content', gap: '24px', ...marginLeftCalc }}>
+          {children}
+        </Box>
+      </Box>
+      <Container id="pnArrowContainer" maxWidth="lg" sx={{ mt: '24px' }}>
+        <Button onClick={() => null} disabled={leftDisabled}>
+          <ArrowBackIosIcon />
         </Button>
-        <Button className="arrow__right" onClick={() => slider?.current?.slickNext()} >
-          <ArrowForwardIosIcon className="arrow__rightIcon" />
+        <Button onClick={() => null} disabled={rightDisabled}>
+          <ArrowForwardIosIcon />
         </Button>
+      </Container>
     </>
   )
 }
 
 export default CardSlider
-"a benevolent smile"
-
--- components/landing/Uses.tsx
+++ components/landing/Uses.tsx
@@ -1,50 +1,128 @@
-import { Grid, Container, Box, Typography, Button, Card, CardContent } from "@mui/material";
+import React, { FC } from "react";
+import { Grid, Container, Box, Typography, Button, Card, CardContent, FabClassKey } from "@mui/material";
 import SectionTitle from "@components/SectionTitle";
 import Blockquote from "@components/Blockquote";
 import CardSlider from "@components/CardSlider";
 import { DarkTheme } from "@theme/theme";
+import Image from "next/image";
+
+const cardTitleStyle = {
+  fontSize: "24px",
+  fontWeight: "700",
+  lineHeight: "32px",
+  fontFamily: '"Space Grotesk", sans-serif',
+  mt: "2px",
+  mb: "16px",
+};
+
+interface MyCardProps {
+  image: string;
+  title: string;
+  body: string;
+}
+
+const MyCard: FC<MyCardProps> = ({ image, title, body }) => {
+  return (
+    <Card sx={{
+      backgroundColor: '#20252f',
+      backgroundImage: 'url("/card-bg.jpg")',
+      width: '460px',
+      maxWidth: 'calc(100vw - 48px)',
+      display: 'inline-flex',
+      whiteSpace: 'normal',
+    }}>
+      <CardContent>
+        <Image src={image} width={35} height={35} />
+        <Typography sx={cardTitleStyle}>{title}</Typography>
+        <Blockquote small>
+          {body}
+        </Blockquote>
+      </CardContent>
+    </Card>
+  )
+}
+
+const theCards = [
+  {
+    title: "Blockchain Development Teams",
+    body: "Crowdsource funding for a project, and launch a DAO to manage the treasury. Give contributors a say in the direction of project development.",
+    image: "/DevIcon.svg",
+  },
+  {
+    title: "Blockchain Development Teams",
+    body: "Crowdsource funding for a project, and launch a DAO to manage the treasury. Give contributors a say in the direction of project development.",
+    image: "/DevIcon.svg",
+  },
+  {
+    title: "Investor Groups",
+    body: "Using a DAO, groups of people can pool their funds and manage each members' stake in the overall investment. Eliminate the need to trust a specific individual with control of the funds.",
+    image: "/ChartIcon.svg",
+  },
+  {
+    title: "Blockchain Development Teams",
+    body: "Crowdsource funding for a project, and launch a DAO to manage the treasury. Give contributors a say in the direction of project development.",
+    image: "/DevIcon.svg",
+  },
+  {
+    title: "Blockchain Development Teams",
+    body: "Crowdsource funding for a project, and launch a DAO to manage the treasury. Give contributors a say in the direction of project development.",
+    image: "/DevIcon.svg",
+  },
+  {
+    title: "Blockchain Development Teams",
+    body: "Crowdsource funding for a project, and launch a DAO to manage the treasury. Give contributors a say in the direction of project development.",
+    image: "/DevIcon.svg",
+  },
+  {
+    title: "Blockchain Development Teams",
+    body: "Crowdsource funding for a project, and launch a DAO to manage the treasury. Give contributors a say in the direction of project development.",
+    image: "/DevIcon.svg",
+  },
+]
 
 export default function Uses() {
   return (
-    <Container sx={{ px: "24px" }}>
-      <Grid container sx={{ py: "120px" }}>
-        <Grid item md={5}>
-          <Grid container spacing={3}>
-            <Grid item>
-              <SectionTitle>
-                Uses
-              </SectionTitle>
-            </Grid>
-            <Grid item>
-              <Typography
-                sx={{
-                  fontSize: "48px",
-                  fontWeight: "400",
-                  lineHeight: "116.7%",
-                  color: DarkTheme.palette.text.primary,
-                  textTransform: "uppercase",
-                  fontFamily: '"Viga", sans-serif',
-                }}
-              >
-                What can you do?&lt;
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Blockquote small>
-                There are a lot of ways to use a DAO. Here are just a few examples of the types of organizations that could launch a DAO on Paideia.
-              </Blockquote>
+    <>
+      <Container sx={{ px: "24px" }}>
+        <Grid container sx={{ pt: "120px", pb: '64px' }}>
+          <Grid item md={5}>
+            <Grid container spacing={3}>
+              <Grid item>
+                <SectionTitle>
+                  Uses
+                </SectionTitle>
+              </Grid>
+              <Grid item>
+                <Typography
+                  sx={{
+                    fontSize: "48px",
+                    fontWeight: "400",
+                    lineHeight: "116.7%",
+                    color: DarkTheme.palette.text.primary,
+                    textTransform: "uppercase",
+                    fontFamily: '"Viga", sans-serif',
+                  }}
+                >
+                  What can you do?&lt;
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Blockquote small>
+                  There are a lot of ways to use a DAO. Here are just a few examples of the types of organizations that could launch a DAO on Paideia.
+                </Blockquote>
+              </Grid>
             </Grid>
           </Grid>
+          <Grid item md={7}></Grid>
         </Grid>
-        <Grid item md={7}></Grid>
-      </Grid>
-        <CardSlider>
-          <Card>
-            <CardContent>
-              Hello
-            </CardContent>
-          </Card>
-        </CardSlider>
-    </Container>
+      </Container>
+      <CardSlider>
+        {
+          theCards.map(({ title, body, image }, i: number) => {
+            return <MyCard key={i} title={title} body={body} image={image} />
+          })
+        }
+      </CardSlider>
+    </>
   )
 }
