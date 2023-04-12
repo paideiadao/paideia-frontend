55337c47121921214fdd47553e9ba42f23d40dc6
esot321c <esot321c@gmail.com>
2023-04-12 12:48:58 -0700

    update crossOrigin

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,7 +22,7 @@ export default class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin="true"
+            crossOrigin="anonymous"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter&family=Space+Grotesk&family=Viga&display=swap"
